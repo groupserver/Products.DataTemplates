dd0d64e7a64ed1c816708d9b79bb3a3de86c9175
damien <none@none>
2003-04-15 03:08:21 +0000

    Changes to load the 4Suite 1.0a1 plugin
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/DataTemplates/trunk%40103

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,6 +7,11 @@ def log_tb(plugin_name):
 plugin_registry = {}
 
 try:
+    import plugin_4suite_1_0a1
+    plugin_4suite_1_0a1.register_plugin(plugin_registry)
+except:
+    log_tb('4suite_1_0a1')
+try:
     import plugin_4suite
     plugin_4suite.register_plugin(plugin_registry)
 except:
