0fdcf933b3f957e1af19e53b2f4f6c5f8cdb5f3c
Richard Waid <richard@iopen.net>
2003-04-19 08:19:07 +0000

    Major feature added -- resolver support.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/DataTemplates/trunk%40108

--- plugins/plugin_libxslt.py
+++ plugins/plugin_libxslt.py
@@ -1,9 +1,40 @@
-import libxslt, libxml2
+import libxslt, libxml2, StringIO
+
+class UriResolver:                                                                   
+    def __init__(self,context=None):
+        self.context = context
+
+    def resolve(self, uri, id, ctxt):
+        import sys
+
+        stream = None
+        if self.context:
+            for pathpart in uri.split('/'):
+                if pathpart in ['..','.','http:']:
+                    # ignore these, acquisition will work anyway
+                    continue
+                obj = getattr(self.context, pathpart, None)
+                # the new context is the object
+                if obj:
+                    self.context = obj
+            if obj:
+                stream = StringIO.StringIO(str(obj()))
+                                                                                
+        if not stream and os.access(uri, os.F_OK):
+            #Hack because urllib breaks on Windows paths
+            stream = open(uri)
+        elif not stream:
+            stream = urllib.urlopen(uri)
+        return stream
+
 def render(self, source_xml, content_type):
     """ Render document using libxslt.
 
     """
     import tempfile, os, StringIO
+
+    resolver = UriResolver(self).resolve
+    libxml2.setEntityLoader(resolver)
     styledoc = libxml2.parseDoc(self())
     style = libxslt.parseStylesheetDoc(styledoc)
     doc = libxml2.parseDoc(source_xml)
