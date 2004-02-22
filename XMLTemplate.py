88039025bfe0354b05bf44632df0924287c3349c
Richard Waid <richard@iopen.net>
2004-02-22 01:38:51 +0000

    Modified the internal terminology, and enabled renderers without templates (like RML Renderer) to be called.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/DataTemplates/trunk%40133

--- XMLTemplate.py
+++ XMLTemplate.py
@@ -12,6 +12,9 @@ from Products.PageTemplates import ZopePageTemplate, PageTemplateFile
 from AccessControl import getSecurityManager, ClassSecurityInfo
 from DTCacheManager import DTCacheManagerAware
 
+class TemplateError(Exception):
+    pass
+
 class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
                   DTCacheManagerAware):
     """ An XML document that may use TAL and METAL to produce itself.
@@ -22,29 +25,30 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
     """
     security = ClassSecurityInfo()
     
-    unselected_xslt = 'do not use xslt'
+    unselected_transform = 'do not use transform'
     unselected_schema = 'do not use schema'
     meta_type = "XML Template"
-
-    version = 1.12
-
+    
+    version = 1.65
+    
     render_methods = ['xml', 'html', 'pdf']
     content_type_map = {'xml': 'text/xml',
                         'html': 'text/html',
                         'pdf': 'application/pdf'}
-
+    
     cache_manager = DTCacheManagerAware.null_cache_manager
 
     default_render_method = ''
-    stylesheet_paths = []
+    transform_container_types = ('RML Renderer', 'XSLT Template')
+    transform_paths = []
     schema_paths = []
     _def_properties = ({'id': 'title', 'type': 'string', 'mode': 'w'},
                        {'id':'cache_manager', 'type': 'selection', 'mode':'w',
                         'select_variable': 'get_cacheManagerIds'},
                        {'id':'default_render_method', 'type': 'selection',
                         'mode': 'w', 'select_variable': 'get_renderMethods'},
-                       {'id':'stylesheet_paths', 'type': 'lines', 'mode': 'w'},
-                       {'id':'schema_paths', 'type': 'lines', 'mode': 'w'}
+                       {'id':'transform_paths', 'type': 'lines', 'mode': 'w'},
+                       {'id':'schema_paths', 'type': 'lines', 'mode': 'w'},
                        )
     _properties = _def_properties
     
@@ -58,7 +62,7 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
         
         self._setPropValue('content_type', 'text/xml')
 
-        self.setup_xsltProperties()
+        self.setup_transformProperties()
         self.setup_schemaProperties()
 
         ZopePageTemplate.ZopePageTemplate.__init__(self, id)
@@ -70,42 +74,73 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
         """ Upgrade to the latest version.
 
         """
+        import re
         currversion = getattr(self, '_version', 0)
         if currversion == self.version:
             return 'already running latest version (%s)' % currversion
+        
+        propertyitems = self.propertyItems()
+        for propertyitem in propertyitems:
+            if propertyitem[0] in ('stylesheet_paths', 'xslt_paths'):
+                self.transform_paths = propertyitem[1]
+            elif propertyitem[0][:4] == 'xslt':
+                transform = getattr(self, propertyitem[1], None)
+                if not transform:
+                    for obj in self._get_path_objs(self.transform_paths):
+                        transform = getattr(obj, propertyitem[1], None)
+                        if transform:
+                            break
+                if transform:
+                    mtype = re.sub('template|renderer', '', transform.meta_type.lower()).strip()            
+                    setattr(self,
+                            'transform_%s' % propertyitem[0][5:],
+                            '%s (%s)' % (propertyitem[1], mtype))
+            elif propertyitem[0][:10] == 'stylesheet':
+                setattr(self, 'template_%s' % propertyitem[0][11:], propertyitem[1])
+            else:
+                continue
+            try:
+                delattr(self, propertyitem[0])
+            except:
+                pass
+            try:
+                self._delProperty(propertyitem[0])
+            except:
+                pass
 
         self._properties = self._def_properties
-        self.setup_xsltProperties()
+        self.setup_transformProperties()
         self.setup_schemaProperties()
-
+        
         self._version = self.version
         
         return 'upgraded %s to version %s from version %s' % (self.getId(),
                                                               self._version,
                                                               currversion)
-    def setup_xsltProperties(self):
-        """ Setup the various XSLT properties.
+        
+    def setup_transformProperties(self):
+        """ Setup the various transform properties.
         
         """
         for method in self.render_methods:
-            curr = getattr(self, 'xslt_%s' % method, '')
+            curr = getattr(self, 'transform_%s' % method, '')
             try:
-                self._delProperty('xslt_%s' % method)
+                self._delProperty('transform_%s' % method)
             except ValueError:
                 pass
             try:
-                delattr(self, 'xslt_%s' % method)
+                delattr(self, 'transform_%s' % method)
             except:
                 pass
-            setattr(self, 'xslt_%s' % method, curr)
+            setattr(self, 'transform_%s' % method, curr)
 
         properties = list(self._properties)
         for method in self.render_methods:
-            properties.append({'id': 'xslt_%s' % method,
+            properties.append({'id': 'transform_%s' % method,
                                'type': 'selection',
                                'select_variable': 'get_templateCandidates',
                                'mode': 'w'})
-
+        
         self._properties = tuple(properties)
 
     def setup_schemaProperties(self):
@@ -127,9 +162,9 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
 
         properties = list(self._properties)
         properties.append({'id': propname,
-                               'type': 'selection',
-                               'select_variable': 'get_schemaCandidates',
-                               'mode': 'w'})
+                           'type': 'selection',
+                           'select_variable': 'get_schemaCandidates',
+                           'mode': 'w'})
 
         self._properties = tuple(properties)
 
@@ -204,24 +239,28 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
         return objs
 
     def get_templateCandidates(self, include_unselected=1):
-        """ Return a tuple of XSLT Containers available for us
+        """ Return a tuple of transform Containers available for us
         to render against.
         
         Optionally specify whether we want the unselected candidate
         option to be included.
         
         """
+        import re
         vals = []
+        objs = list(self.superValues(self.transform_container_types))
+        for path in self._get_path_objs(self.transform_paths):
+            for item in path.superValues(self.transform_container_types):
+                objs.append(item)
+                
+        for obj in objs:
+            mtype = re.sub('template|renderer', '', obj.meta_type.lower()).strip()
+            vals.append('%s (%s)' % (obj.id, mtype))
         
+        vals.sort()
         if include_unselected:
-            vals.append(self.unselected_xslt)
-        for item in self.superValues('XSLT Template'):
-            vals.append(item.id)
-        
-        for obj in self._get_path_objs(self.stylesheet_paths):
-            for item in obj.superValues('XSLT Template'):
-                vals.append(item.id)
-
+            vals.insert(0, self.unselected_transform)
+            
         return tuple(vals)
 
     def get_schemaCandidates(self, include_unselected=1):
@@ -236,9 +275,6 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
         
         if include_unselected:
             vals.append(self.unselected_schema)
-
-        #for item in self.superValues('XML Template'):
-        #    vals.append(item.id)
         
         for obj in self._get_path_objs(self.schema_paths):
             for item in obj.superValues('XML Template'):
@@ -265,7 +301,7 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
     def writeFromHTTPPost(self, REQUEST, RESPONSE=None):
         """ Writes the raw POST data to the template.
         
-        This is implemented specifically for accepting POSTs from
+        This was implemented specifically for accepting POSTs from
         the bitflux editor, hence the specific response codes.
         """
         # Retrieve the template text from the raw form data and call the
@@ -319,7 +355,7 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
         """ Render the document via the given method.
         
         """
-        import urlparse, md5
+        import urlparse, md5, re
         
         request = getattr(self, 'REQUEST', None)
         
@@ -332,29 +368,31 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
         if method not in self.render_methods:
             method = self.default_render_method
         
-        xslt_id = getattr(self, 'xslt_%s' % method, '')
+        transform_id = getattr(self, 'transform_%s' % method, '')
+        transform_id = re.sub('\s\([^\(]*?\)$', '', transform_id)
         
         content_type = self.content_type_map.get(method, 'text/plain')
         # note we make sure we don't have a unicode object at the later steps, because that causes
         # all sorts of headaches with the XML parser later
         xml_rendered = str(self.pt_render(extra_context=extra_context))
-        if not xslt_id or xslt_id == self.unselected_xslt:
+        if not transform_id or transform_id == self.unselected_transform:
             rendered = xml_rendered
         else:
-            xslt = getattr(self, xslt_id, None)
-            if not xslt:
-                for obj in self._get_path_objs(self.stylesheet_paths):
-                    xslt = getattr(obj, xslt_id, None)
-                    if xslt:
+            transform = getattr(self, transform_id, None)
+            if not transform:
+                for obj in self._get_path_objs(self.transform_paths):
+                    transform = getattr(obj, transform_id, None)
+                    if transform:
                         break
-                    
+            if not transform:
+                raise TransformError, 'Transform %s did not exist' % transform_id
             self.prune_cache()
-            cached = self.retrieve_cache(xslt, xml_rendered)
+            cached = self.retrieve_cache(transform, xml_rendered)
             if cached:
                 rendered = cached
             else:
-                rendered = xslt.render_xml(xml_rendered, content_type)
-                self.update_cache(xslt, xml_rendered, rendered, 0)
+                rendered = transform.render_xml(xml_rendered, content_type)
+                self.update_cache(transform, xml_rendered, rendered, 0)
             
         # set the base properly
         pathparts = list(urlparse.urlparse(self.absolute_url()))
@@ -415,7 +453,6 @@ class XMLTemplate(ZopePageTemplate.ZopePageTemplate,
             return result
         finally:
             security.removeContext(self)
-
 #
 # Zope Management Methods
 #
