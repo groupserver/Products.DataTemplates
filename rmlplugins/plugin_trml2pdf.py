800f6374818a27fb1ae51947b16678849f746d22
Richard Waid <richard@iopen.net>
2004-02-22 10:38:52 +0000

    Fix race conditions. Not a pretty fix, uses a full lock
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/DataTemplates/trunk%40140

--- rmlplugins/plugin_trml2pdf.py
+++ rmlplugins/plugin_trml2pdf.py
@@ -6,8 +6,44 @@
 # You MUST follow the rules in README_STYLE before checking in code
 # to the head. Code which does not follow the rules will be rejected.  
 #
-import reportlab.lib.utils
-def imageReaderInit(self, fileName):
+import reportlab.lib.utils, xml
+from trml2pdf import trml2pdf
+
+import ThreadLock
+ 
+_thread_lock = ThreadLock.allocate_lock()
+
+class RMLPluginError(Exception):
+    pass
+
+def _rml_doc__init__(self, data, context):
+    self.dom = xml.dom.minidom.parseString(data)
+    self.context = context
+    self.filename = self.dom.documentElement.getAttribute('filename') 
+trml2pdf._rml_doc.__init__ = _rml_doc__init__
+
+def _image(self, node):
+    from reportlab.lib.utils import ImageReader
+    from trml2pdf import utils
+    img = ImageReader(str(node.getAttribute('file')), self.doc.context)
+    (sx,sy) = img.getSize()
+    args = {}
+    for tag in ('width','height','x','y'):
+        if node.hasAttribute(tag):
+            args[tag] = utils.unit_get(node.getAttribute(tag))
+        if ('width' in args) and (not 'height' in args):
+            args['height'] = sy * args['width'] / sx
+        elif ('height' in args) and (not 'width' in args):
+            args['width'] = sx * args['height'] / sy
+        elif ('width' in args) and ('height' in args):
+            if (float(args['width'])/args['height'])>(float(sx)>sy):
+                args['width'] = sx * args['height'] / sy
+            else:
+                args['height'] = sy * args['width'] / sx
+    self.canvas.drawImage(img, **args)
+trml2pdf._rml_canvas._image = _image
+
+def ImageReader__init__(self, fileName, context):
     import PIL.Image, PIL.ImageFileIO, StringIO
     self._fileName = fileName
     self._image = None
@@ -20,28 +56,37 @@ def imageReaderInit(self, fileName):
         if pathpart == '..' or pathpart == '.':
             # ignore these, acquisition will work anyway
             continue
-        obj = getattr(self.context, pathpart, None)
+        obj = getattr(context, pathpart, None)
         # the new context is the object
         if obj:
-            self.context = obj
+            context = obj
     if obj:
         data = StringIO.StringIO(obj.data)
         size = (obj.width, obj.height)    
         self._image = PIL.Image.open(PIL.ImageFileIO.ImageFileIO(data))
+    else:
+        raise RMLPluginError, 'Image %s could not be found' % fileName
+reportlab.lib.utils.ImageReader.__init__ = ImageReader__init__
 
 def render(self, source_xml, content_type):
     """ Render document using trml2pdf.
 
     """
-    import StringIO, trml2pdf
-
-    reportlab.lib.utils.ImageReader.__init__ = imageReaderInit
-    reportlab.lib.utils.ImageReader.context = self
-    result = trml2pdf.parseString(source_xml)
+    import StringIO
     
-    return result
+    fp = StringIO.StringIO()
+    try:
+        # numerous issues were encountered with multiple thread accesses
+        # so we're going for absolute robustness
+        _thread_lock.acquire()
+        r = trml2pdf._rml_doc(source_xml, self)
+        r.render(fp)
+    finally:
+        _thread_lock.release()
 
+    return fp.getvalue()
+    
 def register_plugin(plugin_registry):
-    plugin_registry['http://iopen.co.nz/plugins/rml/rml2pdf'] = ('rml2pdf',
-                                                                  render)
+    plugin_registry['http://iopen.co.nz/plugins/rml/trml2pdf'] = ('trml2pdf',
+                                                                   render)
 
