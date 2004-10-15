c2e70ed0003445b9ddbe53d9a6f57dda38cade24
Richard Waid <richard@iopen.net>
2004-10-15 04:51:00 +0000

    Modification to point STYLE to new location.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/DataTemplates/trunk%40449

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 #
 # For details of the license, please see LICENSE.
 #
-# You MUST follow the rules in README_STYLE before checking in code
+# You MUST follow the rules in http://iopen.net/STYLE before checking in code
 # to the head. Code which does not follow the rules will be rejected.  
 #
 def initialize(context):
